1b4e6bf3acd4a039f5fe986db6c3fc5f3865921d
martamae <wegnerma@onid.oregonstate.edu>
2015-04-26 11:29:47 -0500

    Added function to input from the search bar and stores info from gistLog in variables

--- Assign3P2.js
+++ Assign3P2.js
@@ -8,12 +8,41 @@ var fetchData = function () {
     req.onreadystatechange = function () {
         if (this.readyState === 4) {
             var gistLog = JSON.parse(this.responseText);
+
+            for (var i = 0; i < gistLog.length; i++) {
+                generateGistHtml(gistLog[i]);
+            }
         }
     };
     req.open('GET', 'https://api.github.com/gists');
     req.send();
 }
 
+var saveInput = function () {
+    var input = document.getElementsByName('input')[0].value;
+}
+
+var generateGistHtml = function (gist) {
+    var id = gist.id;
+    var description = gist.description;
+    var url = gist.url;
+
+    //gist will have the entire gist data that comes from the api,
+    // for the details check my pinned discussion about understanding JSON
+    //Add a button (code above goes here) next to each element and save
+    // the gist id in the html to be able to find it again, if you chose id,
+    // you need a function called findById(id) that takes a gist id and iterates
+    // over originalGistList to find the appropriate gist and returns it.
+    //This function will be used in the previous step function (fetchData)
+}
+
+var findById = function(id) {
+	//iterate over list of gists to find the gist with id equals to input id
+	//return that gist
+}
+
 window.onload = function () {
     fetchData();
+
+    generateGistHtml(document.getElementById('gist'));
 }
